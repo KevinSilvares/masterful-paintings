c7fcbf6a127939c6ac4ee517c03a85da81686935
KevinSilvares <k.silvares24@gmail.com>
2023-11-13 12:05:03 +0100

    Commit inicial

---
+++ Cuadro.java
@@ -0,0 +1,14 @@
+public class Cuadro
+{
+    // instance variables - replace the example below with your own
+    private String autor;
+    private double precio;
+
+    /**
+     * Constructor for objects of class Galeria
+     */
+    public Cuadro(String autor, double precio){
+        this.autor = autor;
+        this.precio = precio;
+    }
+}
