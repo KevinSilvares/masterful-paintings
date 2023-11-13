9cf04ac89e62a8c8de3e428afb1c34cad429d80a
Juan Gutierrez Gutierrez <gurruny1@gmail.com>
2023-11-13 12:23:04 +0100

    Se ha creado la clase Galeria.

---
+++ Galeria.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Galeria here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Galeria
+{
+    // instance variables - replace the example below with your own
+    private String nombre;
+    private int superficie;
+    private Cuadro cuadro;
+
+    /**
+     * Constructor for objects of class Galeria
+     */
+    public Galeria()
+    {
+    }
+
+}
